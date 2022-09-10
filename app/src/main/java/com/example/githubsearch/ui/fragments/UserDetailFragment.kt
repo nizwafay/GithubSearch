b60ebb753507c347a1fbef40c8d63f0c20861c79
Muhammad Nizwa <muhammadnizwa@gmail.com>
2022-09-11 02:52:47 +0700

    init nav graph

---
+++ app/src/main/java/com/example/githubsearch/ui/fragments/UserDetailFragment.kt
@@ -0,0 +1,7 @@
+package com.example.githubsearch.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.githubsearch.R
+
+class UserDetailFragment : Fragment(R.layout.fragment_user_detail) {
+}
---
+++ app/src/main/java/com/example/githubsearch/ui/fragments/UsersSearchFragment.kt
@@ -0,0 +1,7 @@
+package com.example.githubsearch.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.githubsearch.R
+
+class UsersSearchFragment : Fragment(R.layout.fragment_users_search) {
+}
